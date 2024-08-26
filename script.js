880536a5d5b51a16b6f2177c4a08c918d2f64c02
SharnuSharath123 <sharnuburli143@gmail.com>
2024-08-26 22:33:59 +0530

    curly-telegram

---
+++ script.js
@@ -0,0 +1,5 @@
+const icon = document.getElementById('telegram-icon');
+icon.addEventListener('click', () => {
+const path = document.getElementById('icon-path');
+path.setAttribute('fill', '#005b96'); // Change color on click
+});
